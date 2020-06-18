805c6eef88d4ae25bc30df35f10acf43d88a69d6
Jesse <jessejesse123@gmail.com>
2020-06-17 23:59:53 -0400

    Parse cli arguments

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"flag"
 	"log"
+	"strconv"
 
 	"github.com/miekg/dns"
 )
@@ -14,13 +16,29 @@ func listenMessages(messages chan string) {
 }
 
 func main() {
-	srv := &dns.Server{Addr: ":53", Net: "udp"}
-	tun := newTunnel("t1.jse.li.")
-	dns.Handle(".", tun)
+	port := flag.Int("port", 53, "port to run on")
+	flag.Parse()
 
+	if flag.NArg() != 1 {
+		log.Fatal("tunnel accepts exactly one argument for the top domain")
+	}
+
+	tun := newTunnel(flag.Arg(0))
+	dns.Handle(".", tun)
 	go listenMessages(tun.Messages)
 
-	if err := srv.ListenAndServe(); err != nil {
-		log.Fatal(err)
-	}
+	go func() {
+		srv := &dns.Server{Addr: ":" + strconv.Itoa(*port), Net: "udp"}
+		if err := srv.ListenAndServe(); err != nil {
+			log.Fatalf("Failed to set udp listener %s\n", err.Error())
+		}
+	}()
+	go func() {
+		srv := &dns.Server{Addr: ":" + strconv.Itoa(*port), Net: "tcp"}
+		if err := srv.ListenAndServe(); err != nil {
+			log.Fatalf("Failed to set tcp listener %s\n", err.Error())
+		}
+	}()
+
+	select {} // block forever
 }
